0f6df9fa514ffff22c38945f12cd46acf13e3ffd
BerangereHetic <berangere.martin@hetic.net>
2017-01-27 09:56:21 +0100

    User can choose the size of the grid. Other modifications in progress

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-var grid = [];
-var container = document.querySelector('.container');
+var grille = document.querySelector('.grille');
 
-function laby()
-{
-    var x =  prompt("Combien de case voulez-vous ?");
-    for (var i=0; i<x; i++)
-    {
-          container.innerHTML += '<div class="square"><div class="square"></div></div>';
+var x = parseInt(prompt("Entrez un nombre entre 2 et 10 ?"));
+/*var posX = 0;
+var posY = 0;*/
+
+function labyrinthe(){
+    for (var i=0; i<x; i++){
+        grille.innerHTML += '<div class="square"></div>';
     }
+    
+    var square = grille.querySelectorAll('.square');
+    
+    for (var i=0; i<x; i++){
+        for (var j=0; j<x; j++){
+            square[i].innerHTML += '<div class="squareColumn"></div>';
+        }
+    }
+    
 }
 
-laby();
+function randomPos(){
+    return Math.floor(Math.random()*x ) + 1;
+}
+
+function init(){
+    for(var i=0; i<20; i++){
+        do{
+            var posX = randomPos();
+            var posY = randomPos();
+        }while(!((posX==0 && posX==(x-1)) && (posY==0 && posY==(x-1))));
+        console.log('x' + posX);
+        console.log('y' + posY);
+    }
+    
+    
+}
+
+labyrinthe(x);
+init();
