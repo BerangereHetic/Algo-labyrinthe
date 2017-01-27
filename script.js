96da584d9998301083b224137489d9ea80ee8425
BerangereHetic <berangere.martin@hetic.net>
2017-01-27 12:48:05 +0100

    Way prepared

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ var grille = document.querySelector('.grille');
 var x = parseInt(prompt("Entrez un nombre entre 2 et 10 ?"));
 var posX = 0;
 var posY = 0;
+var way = 0;
 
-function init(){
-    
-}
+do{
+    var posX = randomPos();
+    var posY = randomPos();
+}while(!((posX==0 || posX==(x-1)) || (posY==0 || posY==(x-1))));
 
 function labyrinthe(){
-    do{
-        var posX = randomPos();
-        var posY = randomPos();
-    }while(!((posX==1 || posX==x) || (posY==1 || posY==x)));
-    
-    posX--;
-    posY--;
     
     for (var i=0; i<x; i++){
         grille.innerHTML += '<div class="square square-'+i+'"></div>';
@@ -32,8 +27,32 @@ function labyrinthe(){
 }
 
 function randomPos(){
-    return Math.floor((Math.random()*x)+1);
+    return Math.floor((Math.random()*x));
 }
 
-init(x);
-labyrinthe(x, posX, posY);
+function randomWay(){    
+    return Math.floor((Math.random()*4)+1);    
+}
+
+function waySelected(){
+    do{
+        var way = randomWay();
+        console.log('hey');
+    }
+    while(
+        (posX==0 && way==1) || 
+        (posX==(x-1) && way==3) || 
+        (posY==0 && way==2) || 
+        (posY==(x-1) && way==4) ||
+        ((way==1 || way==2) && (posX==0 && posY==0)) ||
+        ((way==1 || way==4) && (posX==0 && posY==(x-1))) ||
+        ((way==2 || way==3) && (posX==(x-1) && posY==0)) ||
+        ((way==3 || way==4) && (posX==(x-1) && posY==(x-1)))
+    );
+    console.log(way);
+    console.log(posX, posY);
+    
+}
+
+waySelected();
+labyrinthe();
